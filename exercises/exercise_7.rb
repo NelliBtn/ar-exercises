f33adcc2344a43dafe163f91236444bd851b13d6
Nelli Butenko <butenkonelly93@gmail.com>
2021-05-25 20:13:41 +0000

    Add validations

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,13 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+# 1. Add validations to two models to enforce the following business rules:
+# 2. Ask the user for a store name (store it in a variable)
+# 3. Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
+# 4. Display the error messages provided back from ActiveRecord to the user (one on each line) after you attempt to save/create the record
+
+puts 'Enter store name'
+store_name = $stdin.gets.chomp
+store = Store.create(name: store_name)
+puts new_store.errors.full_messages
