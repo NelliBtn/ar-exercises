f46b2ebda5bc7758a6c5104c5c431b6c9ff8eb06
Nelli Butenko <butenkonelly93@gmail.com>
2021-05-25 20:01:31 +0000

    Create employees for stores

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,7 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Tom", last_name: "Catovitz", hourly_rate: 40)
+@store2.employees.create(first_name: "Jerry", last_name: "Mousovitz", hourly_rate: 30)
